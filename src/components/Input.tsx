8c0d4f81681ddc3041e927c21ba3e385fc212e4e
simas.usas <simas.usas@zenitech.co.uk>
2024-04-21 13:18:26 +0300

    Added validation for login

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,20 +1,21 @@
 import { InputHTMLAttributes } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {
+interface Props {
   id: string;
   label?: string;
   className?: string;
+  inputProps?: InputHTMLAttributes<HTMLInputElement>;
 }
 
-const Input = ({ label, id, className, ...props }: Props) => (
-  <div className={twMerge('flex flex-col', className)}>
+const Input = ({ label, id, className, inputProps: { ...inputProps }, ...props }: Props) => (
+  <div className={twMerge('flex flex-col', className)} {...props}>
     {label && (
       <label htmlFor={id} className="mb-1">
         {label}
       </label>
     )}
-    <input id={id} className="border border-gray-400 rounded p-2" {...props} />
+    <input id={id} {...inputProps} className={twMerge('border border-gray-400 rounded p-2', inputProps.className)} />
   </div>
 );
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,38 +3,69 @@ import { useAuthContext } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import Input from '../components/Input';
 import Button from '../components/Button';
+import { twMerge } from 'tailwind-merge';
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState<string | null>(null);
+  const [password, setPassword] = useState<string | null>(null);
+  const [error, setError] = useState<boolean>(false);
 
   const { getToken } = useAuthContext();
   const navigate = useNavigate();
 
-  const handleLogin = async () => {
-    await getToken({ username, password });
-    navigate('/');
+  const onLoginClick = async () => {
+    setError(false);
+
+    if (!username) {
+      setUsername('');
+      document.getElementById('username')?.focus();
+      return;
+    }
+
+    if (!password) {
+      setPassword('');
+      document.getElementById('password')?.focus();
+      return;
+    }
+
+    try {
+      await getToken({ username, password });
+      navigate('/');
+    } catch (e) {
+      setError(true);
+    }
   };
 
   return (
-    <div className="flex flex-col shrink w-96 m-4 p-4 bg-slate-50	rounded ">
-      <Input
-        id="username"
-        label="Username"
-        placeholder="Enter username here..."
-        type="text"
-        onChange={(e) => setUsername(e.target.value)}
-        className="mb-2"
-      />
-      <Input
-        id="password"
-        label="Password"
-        placeholder="Enter password here..."
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        className="mb-10"
-      />
-      <Button onClick={handleLogin}>Sign in</Button>
+    <div className="flex flex-col shrink w-96 m-4 p-4 bg-slate-50	rounded">
+      {error && <div className="text-red-500 text-center mb-2">Incorrect username or password – please try again.</div>}
+      <div className="mb-2">
+        <Input
+          id="username"
+          label="Username"
+          inputProps={{
+            placeholder: 'Enter username here...',
+            type: 'text',
+            onChange: (e) => setUsername(e.target.value),
+            className: twMerge('mb-1', username === '' && ' border-red-500'),
+          }}
+        />
+        {username === '' && <div className="text-red-500">Please enter a valid username.</div>}
+      </div>
+      <div className="mb-2">
+        <Input
+          id="password"
+          label="Password"
+          inputProps={{
+            placeholder: 'Enter password here...',
+            type: 'password',
+            onChange: (e) => setPassword(e.target.value),
+            className: twMerge('mb-1', password === '' && ' border-red-500'),
+          }}
+        />
+        {password === '' && <div className="text-red-500">Please enter a valid password.</div>}
+      </div>
+      <Button onClick={onLoginClick}>Sign in</Button>
     </div>
   );
 };
