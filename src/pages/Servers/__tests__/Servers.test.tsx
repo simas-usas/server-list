8c5814c5d822e6628f42764edb1bc25127902861
simas.usas <simas.usas@zenitech.co.uk>
2024-04-22 11:51:24 +0300

    Fixed Servers test

--- src/pages/Servers/__tests__/Servers.test.tsx
+++ src/pages/Servers/__tests__/Servers.test.tsx
@@ -4,7 +4,40 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 vi.mock('@tanstack/react-query', async (importOriginal) => ({
   ...(await importOriginal<typeof import('@tanstack/react-query')>()),
-  useSuspenseQuery: vi.fn().mockReturnValue({ data: [{ name: 'Mordor', distance: '999' }] }),
+  useSuspenseQuery: vi.fn().mockReturnValue({
+    data: [
+      { name: 'Mordor', distance: '999' },
+      { name: 'Rapture', distance: '400' },
+      { name: 'Skyrim', distance: '600' },
+      { name: 'Midgar', distance: '700' },
+      { name: 'Hyrule', distance: '200' },
+      { name: 'Mushroom Kingdom', distance: '800' },
+      { name: 'Pandora', distance: '550' },
+      { name: 'Azeroth', distance: '650' },
+      { name: 'Tamriel', distance: '1000' },
+      { name: 'Silent Hill', distance: '180' },
+      { name: 'City 17', distance: '850' },
+      { name: 'Dunwall', distance: '950' },
+      { name: 'Columbia', distance: '820' },
+      { name: 'Lordran', distance: '300' },
+      { name: 'Zebes', distance: '190' },
+      { name: 'Arkham City', distance: '420' },
+      { name: 'Spira', distance: '770' },
+      { name: 'San Andreas', distance: '640' },
+      { name: 'Athena', distance: '720' },
+      { name: 'Vvardenfell', distance: '600' },
+      { name: 'Inaba', distance: '880' },
+      { name: 'Los Santos', distance: '320' },
+      { name: 'Bionis', distance: '900' },
+      { name: 'Zanarkand', distance: '800' },
+      { name: 'Narnia', distance: '930' },
+      { name: 'Cerberon', distance: '630' },
+      { name: 'Novigrad', distance: '710' },
+      { name: 'Mega City One', distance: '820' },
+      { name: 'Raccoon City', distance: '880' },
+      { name: 'Silent Hill', distance: '310' },
+    ],
+  }),
 }));
 
 describe('Servers', () => {
