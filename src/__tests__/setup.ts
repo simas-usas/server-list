89efd9be784f92f08caf01667690ac120bd03238
simas.usas <simas.usas@zenitech.co.uk>
2024-04-22 15:44:19 +0300

    removed localStorage mock

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -4,29 +4,6 @@ import '@testing-library/jest-dom/vitest';
 
 vi.stubEnv('VITE_API_HOST', 'https://test.com');
 
-const localStorageMock = (() => {
-  let store: { [key: string]: string } = {};
-
-  return {
-    getItem(key: any) {
-      return store[key] || null;
-    },
-    setItem(key: any, value: any) {
-      store[key] = value.toString();
-    },
-    removeItem(key: any) {
-      delete store[key];
-    },
-    clear() {
-      store = {};
-    },
-  };
-})();
-
-Object.defineProperty(window, 'localStorage', {
-  value: localStorageMock,
-});
-
 afterEach(() => {
   cleanup();
 });
